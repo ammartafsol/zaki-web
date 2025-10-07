8c486299759de77961fb0e0d64be2256bd0de565
tanzeela-fe <tanzeela.fe.tafsol@gmail.com>
2025-10-07 14:01:10 +0500

    Enhance styling by adding new color and typography variables, update button styles for improved responsiveness, and refactor HomeTemplate and Header components for better structure and clarity.

--- src/components/Template/Home/index.jsx
+++ src/components/Template/Home/index.jsx
@@ -1,11 +1,39 @@
+"use client";
 import Link from "next/link";
-import styles from "./HomeTemplate.module.css";
+import classes from "./HomeTemplate.module.css";
+import { Container, Row, Col } from "react-bootstrap";
+import { homePageData } from "@/developmentContext/homePage";
+import clsx from "clsx";
+import { useState } from "react";
+import HeroSection from "@/components/molecules/HeroSection";
+import { imageUrl } from "@/resources/utils/helper";
+import Button from "@/components/atoms/Button";
 
 export default function HomeTemplate() {
+  const [data, setData] = useState(homePageData);
+
   return (
-    <main className={styles.main}>
-    <p className="text-center">Example Project</p>
-      <Link href="/components">See Components</Link>
+    <main>
+      {data?.hero && (
+        <div
+          className={clsx(classes.heroSection)}
+          style={{
+            background: `linear-gradient(180deg, rgba(0, 0, 0, 0.40) -31.56%, rgba(0, 0, 0, 0.00) 80.62%), url(${imageUrl(
+              data?.hero?.image) || data?.hero?.image } ) lightgray -80px -25px / 111.111% 128.571% no-repeat`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <Container>
+            <Row>
+              <Col lg={12}>
+                <HeroSection data={data?.hero} />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      )}
     </main>
   );
 }
---
+++ src/developmentContext/homePage.js
@@ -0,0 +1,9 @@
+export const homePageData = {
+  hero: {
+    title:
+      "Entfalte dein Potenzial mit Improself – finde zertifizierte Therapeuten für alternative Heilmethoden",
+    description:
+      "Improself begleitet dich auf deinem Weg zu nachhaltigem Wohlbefinden und persönlichem Wachstum. Unsere Plattform verbindet dich gezielt mit zertifizierten Therapeuten für alternative Heilmethoden und bietet dir eine Auswahl, die genau auf deine Bedürfnisse und Ziele zugeschnitten ist.",
+    image: "/app-images/banner-image.png",
+  },
+};
